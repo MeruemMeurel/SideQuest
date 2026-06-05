07ee5481bcbfc6afe30b5d2f85df8070da60301f
Manuel <93267191+meruemmeurel@users.noreply.github.com>
2026-06-05 14:17:23 +0200

    feat: add social app and Post model

---
+++ social/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'created_at', 'updated_at',)
+
+    search_fields = ('author__username', 'content',)
+
+    list_filter = ("created_at",)
+
+
