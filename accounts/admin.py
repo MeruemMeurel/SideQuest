c8fd70e00cceb77920946267957c887c94422238
Manuel <93267191+meruemmeurel@users.noreply.github.com>
2026-06-05 02:36:36 +0200

    feat: initialize Django project with custom user model

---
+++ accounts/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ('created_at',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "SideQuest profile",
+            {
+                "fields": (
+                    "bio",
+                    "created_at",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "SideQuest profile",
+            {
+                "fields": ("bio",),
+            },
+        ),
+    )
+
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'created_at',)
+
+    search_fields = ('username', 'email',)
